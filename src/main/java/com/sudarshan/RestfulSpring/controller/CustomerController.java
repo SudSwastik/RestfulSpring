83c88c667b82f7f4e1f6ebf184a1d9c56d75dbcc
SudarshanKumar <sudarshan4309@gmail.com>
2019-11-11 19:49:03 +0530

    Created a Customer Model

--- src/main/java/com/sudarshan/RestfulSpring/controller/CustomerController.java
+++ src/main/java/com/sudarshan/RestfulSpring/controller/CustomerController.java
@@ -1,17 +1,25 @@
 package com.sudarshan.RestfulSpring.controller;
-
+import com.sudarshan.RestfulSpring.models.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
 
-
     @GetMapping("/customers")
-    public String getAllCustomers(){
+    public List<Customer> getAllCustomers(){
+        Customer c1 = new Customer();
+        c1.setId(1);
+        c1.setName("sud");
 
-        return "All Customers";
+        Customer c2 = new Customer();
+        c2.setId(2);
+        c2.setName("sud1");
+        return Arrays.asList(c1,c2);
     }
 }
---
+++ src/main/java/com/sudarshan/RestfulSpring/models/Customer.java
@@ -0,0 +1,81 @@
+package com.sudarshan.RestfulSpring.models;
+
+public class Customer {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String city;
+    private String state;
+    private String country;
+    private String gender;
+
+    public Customer() {
+    }
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
