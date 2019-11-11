c9fe038828c037136ead7dab5937045d59864a1f
SudarshanKumar <sudarshan4309@gmail.com>
2019-11-11 18:38:42 +0530

    Created a Customer Rest Controller

--- src/main/java/com/sudarshan/RestfulSpring/controller/CustomerController.java
+++ src/main/java/com/sudarshan/RestfulSpring/controller/CustomerController.java
@@ -1,4 +1,17 @@
 package com.sudarshan.RestfulSpring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class CustomerController {
+
+
+    @GetMapping("/customers")
+    public String getAllCustomers(){
+
+        return "All Customers";
+    }
 }
